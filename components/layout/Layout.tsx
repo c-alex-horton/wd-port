24f72a1408a867cc0097a8eda8662561e1f76d27
Caleb Horton <c.alex.horton@gmail.com>
2022-12-05 16:28:46 -0600

    Finished Layout Styles

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -4,13 +4,8 @@ import Header from './Header/Header'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='min-h-screen relative flex flex-col items-center text-calb-pink'>
-      <div
-        className='fixed w-full h-screen -z-10'
-        style={{
-          backgroundImage:
-            'linear-gradient(to right top, #681766, #511451, #4b104b, #361137, #2d102e, #2c102e, #2c102f, #2b102f, #331239, #3a1444, #421750, #49195c)',
-        }}></div>
+    <main className={styles.main}>
+      <div className={styles.bg}></div>
       <div className={styles.backgroundAnimation}>
         <ul>
           <li></li>
