1ae6fbde6ad8b523c022efcd58fda72ffa4d8721
Caleb Horton <c.alex.horton@gmail.com>
2022-12-05 13:17:08 -0600

    Swtiched Header styles to modules

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Header.module.scss'
 
 const Header = () => {
   return (
-    <nav className='bg-black bg-opacity-40 flex justify-center sticky p-3 w-full top-0 backdrop-blur-md'>
-      <ul className='flex justify-around w-1/2'>
+    <nav className={styles.header}>
+      <ul>
         <li>About</li>
         <li>Projects</li>
         <li>Work</li>
