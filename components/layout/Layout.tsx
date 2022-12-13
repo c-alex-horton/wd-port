1f8b1453fda5c7988863c8d46bdd94253f469908
Caleb Horton <c.alex.horton@gmail.com>
2022-12-13 09:55:52 -0600

    Added Head and meta data for SEO and Accessibility

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,28 +1,42 @@
 import React from 'react'
 import styles from './Layout.module.scss'
 import Header from './Header/Header'
+import Head from 'next/head'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className={styles.main}>
-      <div className={styles.bg}></div>
-      <div className={styles.backgroundAnimation}>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-      <Header />
-      {children}
-    </main>
+    <>
+      <Head>
+        <title>Caleb Horton - Web Developer</title>
+        <meta
+          name='description'
+          content='Experienced in modern web development practices and design.'
+        />
+        <meta
+          name='keywords'
+          content='React, TypeScript, Web, Developer, Frontend, javascript'
+        />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.bg}></div>
+        <div className={styles.backgroundAnimation}>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <Header />
+        {children}
+      </main>
+    </>
   )
 }
 
